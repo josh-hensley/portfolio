6f18a8c2594888980083e6bee09833fa7009ddd9
josh-hensley <me@joshhensley.com>
2025-02-17 16:56:47 -0600

    altered home page to include links to other routes

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -11,6 +11,11 @@ const Home: React.FC = ()=>{
             BFA in Theatre | 
             Certified in Full Stack Development
         </p>
+        <div className="card-container">
+            <a href="/About"><div className="card">About</div></a>
+            <a href="/Contact"><div className="card">Contact</div></a>
+            <a href="/Projects"><div className="card">Projects</div></a>
+        </div>
     </main>
     )
 }
