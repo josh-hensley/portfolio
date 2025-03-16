fd541a9ae4b8d917ea3128623f0b3970abdf2df9
josh-hensley <me@joshhensley.com>
2025-03-16 18:32:15 -0500

    added acting/directing portfolio

--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -1,5 +1,6 @@
 import snippetScreenshot from '../../assets/Snippet_Screenshot.png'
 import colorMyWorldScreenshot from '../../assets/color-my-world.png'
+import joshhensleyScreenshot from '../../assets/jhcomscreenshot.png'
 
 const Projects: React.FC = () => {
     return (
@@ -12,6 +13,10 @@ const Projects: React.FC = () => {
             <div className="card">
                 <h2>Snippet</h2>
                 <a href="https://snippet-vzjo.onrender.com/" target='_blank'><img className='screenshot' src={ snippetScreenshot } alt="Color my world screenshot" /></a>
+            </div>
+            <div className="card">
+                <h2>joshhensley.com</h2>
+                <a href="https://joshhensley.com/" target='_blank'><img src={ joshhensleyScreenshot } alt="joshhensley.com screenshot" /></a>
             </div>
         </div>
     </main>
